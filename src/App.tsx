fc6604f4559cf6198eb2b0516e99ff6937f04e9a
whateveriiwant <yhjung678@gmail.com>
2025-04-09 14:57:19 +0900

    fix: 밑에 여백 남던 문제

--- src/App.tsx
+++ src/App.tsx
@@ -14,16 +14,19 @@ import Bold from "./assets/bold.svg?react";
 import Underline from "./assets/underline.svg?react";
 import AlignLeft from "./assets/alignLeft.svg?react";
 import Palette from "./assets/palette.svg?react";
+import GithubFooter from "./assets/footer/githubFooter.svg?react";
+import LinkedInFooter from "./assets/footer/linkedinFooter.svg?react";
+import VelogFooter from "./assets/footer/velogFooter.svg?react";
 
 const App = () => {
   return (
-    <div className="relative w-full h-screen">
+    <div className="relative w-full min-h-screen overflow-hidden flex items-center justify-center">
       <div
-        className="absolute inset-0 h-full bg-cover w-full -z-10 blur-xs scale-200"
+        className="absolute inset-0 bg-cover w-full h-full -z-10 blur-xs scale-400"
         style={{ backgroundImage: `url(${bgImage})` }}
       />
-      <div className="flex items-center justify-center">
-        <div className="bg-white px-20 pt-11 mt-10 w-[55rem] h-[70rem] z-10 flex justify-start rounded-[3.5rem] flex-col">
+      <div className="flex items-center justify-center py-10">
+        <div className="bg-white px-20 py-11 w-[55rem] z-10 flex justify-start rounded-[3.5rem] flex-col">
           <img src={thumbnail} alt="thumbnail" className="h-[27rem]" />
           {/* Thumbnail */}
           <div className="flex flex-row items-center justify-between mt-3">
@@ -100,7 +103,8 @@ const App = () => {
               </div>
             </div>
           </div>
-          <div className="flex flex-row items-start mt-20 justify-between">
+          <div className="flex flex-row items-start mt-10 justify-between">
+            {/* 배경 + 텍스트 묶음 */}
             <div className="flex flex-col items-start justify-between">
               <div className="flex flex-row items-start -ml-1">
                 <BackGround width="2.4rem" height="2.4rem" />
@@ -153,6 +157,39 @@ const App = () => {
                   <Palette width="1.8rem" height="1.8rem" />
                 </div>
               </div>
+            </div>
+          </div>
+          <div className="flex flex-row mt-10 items-end mb-2">
+            <div className="flex flex-col items-start">
+              <p className="text-2xl font-semibold text-[#C5C3C3]">
+                Thumbs Up! 👍
+              </p>
+              <p className="text-sm font-extralight text-[#C5C3C3]">
+                Your Best Thumbnail Maker
+              </p>
+              <p className="text-md font-light text-[#C5C3C3]">
+                Made by Seungjun Jeong With ❤️
+              </p>
+            </div>
+            <div className="flex flex-row ml-3 justify-between items-center w-[7.5rem]">
+              <GithubFooter
+                fill="#C5C3C3"
+                width="2.5rem"
+                height="2.5rem"
+                className="transition-colors duration-200 ease-in-out hover:cursor-pointer hover:fill-[#24292E]"
+              />
+              <LinkedInFooter
+                fill="#C5C3C3"
+                width="2.5rem"
+                height="2.5rem"
+                className="-ml-[0.24rem] transition-colors duration-200 ease-in-out hover:cursor-pointer hover:fill-[#0077B5]"
+              />
+              <VelogFooter
+                fill="#C5C3C3"
+                width="1.9rem"
+                height="1.9rem"
+                className="transition-colors duration-200 ease-in-out hover:cursor-pointer hover:fill-[#20C997]"
+              />
             </div>
           </div>
         </div>
