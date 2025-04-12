a6e621dd1ce45f6f36d4819c49d541eeb7467e5c
whateveriiwant <yhjung678@gmail.com>
2025-04-12 21:38:31 +0900

    refactor: prettier ignore

--- src/App.tsx
+++ src/App.tsx
@@ -583,22 +583,19 @@ const App = () => {
               </p>
             </div>
             <div className="flex flex-row ml-3 justify-between items-center w-[7.5rem]">
+              {/* prettier-ignore */}
               <GithubFooter
-                fill="#C5C3C3"
-                width="2.5rem"
-                height="2.5rem"
+                fill="#C5C3C3" width="2.5rem" height="2.5rem"
                 className="transition-colors duration-200 ease-in-out hover:cursor-pointer hover:fill-[#24292E]"
               />
+              {/* prettier-ignore */}
               <LinkedInFooter
-                fill="#C5C3C3"
-                width="2.5rem"
-                height="2.5rem"
+                fill="#C5C3C3" width="2.5rem" height="2.5rem"
                 className="-ml-[0.24rem] transition-colors duration-200 ease-in-out hover:cursor-pointer hover:fill-[#0077B5]"
               />
+              {/* prettier-ignore */}
               <VelogFooter
-                fill="#C5C3C3"
-                width="1.9rem"
-                height="1.9rem"
+                fill="#C5C3C3" width="1.9rem" height="1.9rem"
                 className="transition-colors duration-200 ease-in-out hover:cursor-pointer hover:fill-[#20C997]"
               />
             </div>
