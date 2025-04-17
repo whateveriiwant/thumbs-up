d47fbca3454be06e5e7b174ee3abc89704ebb74e
whateveriiwant <jsj9620@hufs.ac.kr>
2025-04-17 23:11:04 +0900

    fix: 빌드 오류 수정

--- src/components/footer/Footer.tsx
+++ src/components/footer/Footer.tsx
@@ -23,11 +23,12 @@ const Footer = () => {
           height="2.5rem"
           className="-ml-[0.24rem] transition-colors duration-200 ease-in-out hover:cursor-pointer hover:fill-[#0077B5]"
         />
-        {/* prettier-ignore */}
         <VelogFooter
-                fill="#C5C3C3" width="1.9rem" height="1.9rem"
-                className="transition-colors duration-200 ease-in-out hover:cursor-pointer hover:fill-[#20C997]"
-              />
+          fill="#C5C3C3"
+          width="1.9rem"
+          height="1.9rem"
+          className="transition-colors duration-200 ease-in-out hover:cursor-pointer hover:fill-[#20C997]"
+        />
       </div>
     </div>
   );
