aee0ed0903d2ccfa5c31fc3bb31545335730e83d
whateveriiwant <jsj9620@hufs.ac.kr>
2025-04-17 23:13:59 +0900

    fix: 빌드 오류 수정

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,20 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import tailwindcss from '@tailwindcss/vite'
-import svgr from 'vite-plugin-svgr'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import tailwindcss from "@tailwindcss/vite";
+import svgr from "vite-plugin-svgr";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(),
+  plugins: [
+    react(),
     tailwindcss(),
-    svgr(),
+    svgr({
+      // Configure svgr to properly handle SVG imports with ?react query
+      svgrOptions: {
+        exportType: "named",
+        ref: true,
+      },
+      include: "**/*.svg",
+    }),
   ],
-})
+});
