94231a27d395c5b46e904fa97b7d37163b891a6f
whateveriiwant <jsj9620@hufs.ac.kr>
2025-04-17 23:46:44 +0900

    fix: 빌드 오류 수정

--- src/components/footer/Footer.tsx
+++ src/components/footer/Footer.tsx
@@ -11,24 +11,30 @@ const Footer = () => {
         <p className="text-md font-light text-[#C5C3C3]">Made by Seungjun Jeong With ❤️</p>
       </div>
       <div className="flex flex-row ml-3 justify-between items-center w-[7.5rem]">
-        <GithubFooter
-          fill="#C5C3C3"
-          width="2.5rem"
-          height="2.5rem"
-          className="transition-colors duration-200 ease-in-out hover:cursor-pointer hover:fill-[#24292E]"
-        />
-        <LinkedInFooter
-          fill="#C5C3C3"
-          width="2.5rem"
-          height="2.5rem"
-          className="-ml-[0.24rem] transition-colors duration-200 ease-in-out hover:cursor-pointer hover:fill-[#0077B5]"
-        />
-        <VelogFooter
-          fill="#C5C3C3"
-          width="1.9rem"
-          height="1.9rem"
-          className="transition-colors duration-200 ease-in-out hover:cursor-pointer hover:fill-[#20C997]"
-        />
+        <a href="https://github.com/whateveriiwant/thumbs-up" target="_blank" rel="noopener noreferrer">
+          <GithubFooter
+            fill="#C5C3C3"
+            width="2.5rem"
+            height="2.5rem"
+            className="transition-colors duration-200 ease-in-out hover:cursor-pointer hover:fill-[#24292E]"
+          />
+        </a>
+        <a href="https://www.linkedin.com/in/seungjun-dev/" target="_blank" rel="noopener noreferrer">
+          <LinkedInFooter
+            fill="#C5C3C3"
+            width="2.5rem"
+            height="2.5rem"
+            className="-ml-[0.24rem] transition-colors duration-200 ease-in-out hover:cursor-pointer hover:fill-[#0077B5]"
+          />
+        </a>
+        <a href="https://velog.io/@jsj9620" target="_blank" rel="noopener noreferrer">
+          <VelogFooter
+            fill="#C5C3C3"
+            width="1.9rem"
+            height="1.9rem"
+            className="transition-colors duration-200 ease-in-out hover:cursor-pointer hover:fill-[#20C997]"
+          />
+        </a>
       </div>
     </div>
   );
